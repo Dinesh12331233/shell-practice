9158f65e4339d01ea5844ea39bed8406e3351de7
Dinesh Kancherla <dineshkancherla8@gmail.com>
2025-06-16 16:23:40 +0530

    variables dinesh and deepthi

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+PERSON1=dinesh
+PERSON2=deepthi
+
+echo "$PERSON1: hey $PERSON2, how are you?"
+echo "$PERSON2: hello $PERSON1, I am good"
+echo "$PERSON1: I love you $PERSON2"
+echo "$PERSON2: love you too $PERSON1"

528c3be21b6f4ec6cc2d3795a5eb18bf43eec18f
Dinesh Kancherla <dineshkancherla8@gmail.com>
2025-06-16 16:49:50 +0530

    arguments

---
+++ 04-arguments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+PERSON1=Karthik
+PERSON2=Priya
+
+echo "$PERSON1: hey $PERSON2, how are you?"
+echo "$PERSON2: hello $PERSON1, I am good"
+echo "$PERSON1: I love you $PERSON2"
+echo "$PERSON2: love you too $PERSON1"

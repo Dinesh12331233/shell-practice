329d11a6814bad25dbf27d9def3f46aba9095539
Dinesh Kancherla <dineshkancherla8@gmail.com>
2025-06-16 18:20:46 +0530

    movies

---
+++ 07-array.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+MOVIES=("court" "HIT3" "Pushpa2" "devara")
+
+echo "first movie is: ${MOVIE[0]}"
+
+echo "second movie is: ${MOVIE[1]}"
+
+echo "Third movie is: ${MOVIE[2]}"
+
+echo "fourth movie is: ${MOVIE[3]}"
+
+echo "all movies are: ${MOVIES[@]}"

386df5b756fe0fb9702e83ad05b80da45ea95bb4
Dinesh Kancherla <dineshkancherla8@gmail.com>
2025-06-25 12:29:28 +0530

    backup

--- 20-backup.sh
+++ 20-backup.sh
@@ -58,12 +58,12 @@ fi
 
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS) 
 
-if [ ! -z $FILES ]
+if [ ! -z "$FILES" ]
 then 
     echo "files to zip are: $FILES"
     TIMESTAMP=$(date +%F-%H-%M-%S) 
     ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip" 
-    find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ $ZIP_FILE  
+    find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "$ZIP_FILE"  
 
     if [ -f $ZIP_FILE ]
     then 

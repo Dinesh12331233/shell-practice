de4760719e6972319d4451b4edc51add8628c7ed
Dinesh Kancherla <dineshkancherla8@gmail.com>
2025-06-25 10:29:16 +0530

    backup

--- 20-backup.sh
+++ 20-backup.sh
@@ -56,3 +56,11 @@ then
     exit 1
 fi
 
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS) 
+
+if [ ! -z $FILES_TO_DELETE ]
+then 
+    echo "files found"
+else 
+    echo -e "No log files found olderthan 14 days $Y SKIPPING $N" 
+fi 
